e9bb142a51de7ef2aaa9a956871db232fb1ebaf6
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-31 08:55:01 -0600

    Updates

---
+++ precache-manifest.17a51019f1ec959e3e54b56aedc7d9de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ea29c89667c06c31dd6818c44f7a85",
+    "url": "/cryptocurrency-hooks-pranctices/index.html"
+  },
+  {
+    "revision": "e36aa78904bcbcd504a9",
+    "url": "/cryptocurrency-hooks-pranctices/static/css/main.7509406b.chunk.css"
+  },
+  {
+    "revision": "974f9c7f61f38e326470",
+    "url": "/cryptocurrency-hooks-pranctices/static/js/2.bb7d2947.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/cryptocurrency-hooks-pranctices/static/js/2.bb7d2947.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e36aa78904bcbcd504a9",
+    "url": "/cryptocurrency-hooks-pranctices/static/js/main.de2e5aed.chunk.js"
+  },
+  {
+    "revision": "19eb32b0ad1dc119a22e",
+    "url": "/cryptocurrency-hooks-pranctices/static/js/runtime-main.0c3326fb.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/cryptocurrency-hooks-pranctices/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
